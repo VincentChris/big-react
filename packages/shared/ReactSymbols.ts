0ccbfaa266c13d4bc9bf9441899bc6fe12832aa0
vincent.xiao <vincentchris@126.com>
2024-01-27 17:23:10 +0800

    chore: 项目重新开始

--- packages/shared/ReactSymbols.ts
+++
@@ -1,5 +0,0 @@
-const isSupportSymbol = typeof Symbol === 'function' && Symbol.for;
-export const REACT_ELEMENT_TYPE = isSupportSymbol
-	? Symbol.for('React.Element')
-	: 0xeac7;
-export type ReactElementSymbolType = typeof REACT_ELEMENT_TYPE;
